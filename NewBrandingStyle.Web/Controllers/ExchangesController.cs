0f3bea8c160587b5512b4eee70245654bf240cf9
paweldlugosz <48649957+paweldlugosz@users.noreply.github.com>
2020-10-23 18:47:16 +0200

    Labolatoria 1

---
+++ NewBrandingStyle.Web/Controllers/ExchangesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewBrandingStyle.Web.Controllers
+{
+    public class ExchangesController : Controller
+    {
+        public IActionResult Display(int id)
+        {
+            return View();
+        }
+    }
+}

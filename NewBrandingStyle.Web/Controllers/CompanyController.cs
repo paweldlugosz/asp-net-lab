c2591a15a97cce95d41649854bccbd6adf51a332
paweldlugosz <48649957+paweldlugosz@users.noreply.github.com>
2020-11-14 18:36:02 +0100

    Add company form

---
+++ NewBrandingStyle.Web/Controllers/CompanyController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NewBrandingStyle.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewBrandingStyle.Web.Controllers
+{
+    public class CompanyController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(CompanyModel company)
+        {
+            var model = new CompanyAddViewModel()
+            {
+                NumberOfCharsInName = company.Name.Length,
+                NumberOfCharsInDescription = company.Description.Length,
+                isHidden = !company.isVisible
+            };
+            return View("Company Added", model);
+        }
+
+    }
+}
---
+++ NewBrandingStyle.Web/Models/CompanyAddViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewBrandingStyle.Web.Models
+{
+    public class CompanyAddViewModel
+    {
+        public int NumberOfCharsInName { get; set; }
+        public int NumberOfCharsInDescription { get; set; }
+        public bool isHidden { get; set; }
+    }
+}

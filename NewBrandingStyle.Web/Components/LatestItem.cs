d3819471a0ca0de3337deef1c6860f231545ceb8
paweldlugosz <48649957+paweldlugosz@users.noreply.github.com>
2020-11-29 17:44:49 +0100

    Add custom component

---
+++ NewBrandingStyle.Web/Components/LatestItem.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NewBrandingStyle.Web.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewBrandingStyle.Web.Components
+{
+    public class LatestItem : ViewComponent
+    {
+        private readonly ExchangesDbContext dbContex;
+
+        public LatestItem(ExchangesDbContext dbContex)
+        {
+            this.dbContex = dbContex;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var lastItem = dbContex.Items.OrderByDescending(x => x.Id).First();
+            return View(lastItem);
+        }
+    }
+}
